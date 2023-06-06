58fc2a6b833b2658341e316366bdc95603d0cf5e
Vineet Sharma <vsharma@objectsharp.com>
2023-06-06 14:58:53 -0400

    more tries

--- modules/linuxvmhub/variables.tf
+++ modules/linuxvmhub/variables.tf
@@ -1,26 +1,33 @@
 variable "rgname" {
   description = "Resource Group to deploy VM into"
+  type        = string
 }
 variable "location" {
   description = "Resource Group location to deploy to"
+  type        = string
 }
 
 variable "vmname" {
   description = "Name of the VM"
+  type        = string
 }
 variable "subnetid" {
   description = "ID of the subnet to use for the VM deployment"
+  type        = string
 }
 
 variable "vmsize" {
   description = "size of VM"
   default     = "Standard_DS2_v2"
+  type        = string
 }
 
 variable "vmpassword" {
   description = "Password for the VM"
+  type        = string
 }
 
 variable "adminusername" {
   description = "Name of the admin account"
+  type        = string
 }
--- modules/networkbuild/variables.tf
+++ modules/networkbuild/variables.tf
@@ -1,36 +1,43 @@
 variable "vnet_name" {
   description = "Name of the vnet to create"
   default     = "acctvnet"
+  type        = string
 }
 
 variable "resource_group_name" {
   description = "Default resource group name that the network will be created in."
   default     = "myapp-rg"
+  type        = string
 }
 
 variable "location" {
   description = "The location/region where the core network will be created. The full list of Azure regions can be found at https://azure.microsoft.com/regions"
+  type        = string
 }
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
   default     = "10.0.0.0/16"
+  type        = string
 }
 
 # If no values specified, this defaults to Azure DNS 
 variable "dns_servers" {
   description = "The DNS servers to be used with vNet."
   default     = []
+  type        = list(string)
 }
 
 variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
   default     = ["10.0.1.0/24"]
+  type        = list(string)
 }
 
 variable "subnet_names" {
   description = "A list of public subnets inside the vNet."
   default     = ["subnet1"]
+  type        = list(string)
 }
 
 variable "tags" {
